08fb883af458c33e121b79a5bf031a4e8dcf44ff
Platinum <tomrod04@gmail.com>
2025-06-06 14:30:08 +0200

    viernes

---
+++ app/components/ItemProyecto.tsx
@@ -0,0 +1,13 @@
+import { Card } from "@chakra-ui/react";
+
+function IndiceProyecto( id: String, nombre: String ) {
+  return (
+    <Card.Root>
+      <Card.Header />
+      <Card.Body />
+      <Card.Footer />
+    </Card.Root>
+  );
+}
+
+export default IndiceProyecto;
